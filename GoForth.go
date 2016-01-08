d1a6d93c8499370e71b159645406231037872bff
quinnjn <quinn@neumiiller.com>
2016-01-08 10:32:37 -0800

    Looping, printing prompt, taking input

---
+++ GoForth.go
@@ -0,0 +1,49 @@
+package main 
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+)
+
+/** Version of GoForth */
+const VERSION string = "0.0.0";
+
+/** Prints the MOTD */
+func printMotd() {
+  fmt.Println("GoForth " + VERSION);
+}
+
+/** Setup when the program begins */
+func setup() {
+  printMotd();
+}
+
+/** Builds a prompt */
+func prompt() string {
+  return "FORTH> ";
+}
+
+func parse(input string) {
+  //TODO parse the input
+}
+
+/** Loops collecting information until `die` */
+func loop() {
+  var prompt string = prompt();
+  scanner := bufio.NewScanner(os.Stdin)
+
+  fmt.Print(prompt);
+  for scanner.Scan() {
+      var input = scanner.Text();
+      parse(input);
+      fmt.Print(prompt);
+  }
+
+}
+
+/** Starts the FORTH interrupter */
+func main() {
+  setup();
+  loop();
+}
